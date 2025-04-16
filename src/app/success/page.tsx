363cadd116400a90ff261313df31247cfed0d72f
okamoto haruhito <okamotoharuhito@okamotonoAir.lan>
2025-04-16 22:38:21 +0900

    ”最新に更新”

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,26 +1,16 @@
 "use client";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import axios from "axios";
+
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function SuccessPage() {
   const searchParams = useSearchParams();
-  const reservationId = searchParams.get("reservationId");
-
-  useEffect(() => {
-    if (reservationId) {
-      axios
-        .patch(`/api/reservations/${reservationId}`, { paid: true }) 
-        .then(() => console.log(" 予約データを更新しました"))
-        .catch((error) => console.error(" 予約データ更新エラー:", error));
-    }
-  }, [reservationId]);
-
+  const sessionId = searchParams.get("session_id");
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">支払いが完了しました！</h1>
       <p>予約が確定しました。ありがとうございました！</p>
+      <p>ID:{sessionId}</p>
       <Link href="/" className="mt-4 text-blue-500">ホームに戻る</Link>
     </div>
   );
