5a30573a2d79d3b582ac3294c7f349a9456591c0
okamoto haruhito <okamotoharuhito@okamotonoAir.lan>
2025-04-23 22:35:06 +0900

    ”最新に更新”

--- src/app/components/UserMenue.tsx
+++ src/app/components/UserMenue.tsx
@@ -7,16 +7,16 @@ export default async function UserMenue() {
   const user = await getSessionUser(); 
   return (
     <div className=' fixed bottom-0 py-2  bg-white z-50 w-full'>
-      <ul className='flex justify-around border-t border-gray-400 '>
-         <li className='hover:underline'><Link href={"/favoritesPage"}>お気に入り</Link></li>
-         <li className='hover:underline'><Link href={"/userOption"}>アカウント</Link></li>
-         <li className='hover:underline'><Link href={"/messages"}>メッセージ</Link></li>
-         <li className='hover:underline'><Link href={"/reservations"}>予約リスト</Link></li>
+      <ul className='flex justify-center gap-3 md:justify-around md:gap-0 flex-wrap border-t bborder-gray-400 '>
+         <li className='hover:underline '><Link href={"/favoritesPage"} className='text-xs md:text-base'>お気に入り</Link></li>
+         <li className='hover:underline '><Link href={"/userOption"} className='text-xs md:text-base'>アカウント</Link></li>
+         <li className='hover:underline '><Link href={"/messages"} className='text-xs md:text-base'>メッセージ</Link></li>
+         <li className='hover:underline '><Link href={"/reservations"} className='text-xs md:text-base'>予約リスト</Link></li>
          {user?.role === "ADMIN" && 
-            <li><Link href="/admin">アプリ管理者ページ</Link>
+            <li><Link href="/admin"  className='text-xs md:text-base'>管理者ページ</Link>
             </li>}
             {user?.role === "HOST" && 
-            <li><Link href="/host">宿泊先オーナーページ</Link>
+            <li><Link href="/host"  className='text-xs md:text-base'>オーナーページ</Link>
             </li>}
          </ul>
     </div>
